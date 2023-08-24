c32a0eedb5d59aafeab9089ff5ae2c03911a09a4
tim-labahn <tim.labahn@freaknet.de>
2023-08-24 14:25:22 +0200

    more pages!

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,66 @@
 <template>
-  <div v-if="route === 'home'">tst</div>
-  <Form v-else></Form>
+  <div style="margin-top: 120px">
+    <img class="logo" src="SportButler_Logo_Smal_White.png" style="width: 50px; position: absolute; top: 10px; left: 120px; height: auto" />
+    <div class="header">
+      <table>
+        <td>
+          <tr>
+            <a id="Home" @click="route = 'home'">Home</a>
+            <a id="Form" @click="route = 'form'">Form</a>
+            <a id="Login" @click="route = 'login'">Login</a>
+          </tr>
+        </td>
+        <a class="acc" id="Profile" @click="route = 'profile'">👤</a>
+      </table>
+    </div>
+    <img class="headImage" src="background.png" />
+    <div v-if="route === 'home'">
+      <Home></Home>
+    </div>
+    <div v-else-if="route === 'form'">
+      <Form></Form>
+    </div>
+    <div v-else-if="route === 'login'">
+      <Login></Login>
+    </div>
+  </div>
 </template>
+
 <script lang="ts" setup>
-const route = ref('homfe');
 import { ref } from 'vue';
 import Form from './components/Form.vue';
+import Home from './components/Home.vue';
+import Login from './components/Login.vue';
+const route = ref('home');
 </script>
+
+<style scoped>
+.header {
+  position: absolute;
+  left: 60%;
+  top: 0%;
+  color: whitesmoke;
+  float: left;
+  padding: 14px 16px;
+  font-size: 17px;
+  margin: 0;
+}
+a {
+  float: left;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+a:hover {
+  /* background-color: #172531; */
+  /* background-color: #213547; */
+  background-color: #2a455c;
+  user-select: none;
+}
+.acc {
+  border-radius: 30px;
+  border: 1px solid white;
+  background-color: #424242;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="welcome">
+    <h1>Herzlich willkommen bei SportButler!</h1>
+  </div>
+  <div class="welcomeText">
+    <p>
+      Wir freuen uns, dich auf unserer Plattform begrüßen zu dürfen, wo die Organisation und Abrechnung von Sportevents so einfach ist wie nie zuvor.
+    </p>
+
+    <br />
+    <h3>SportButler, ist dein persönlicher Sport-/Event-Organisator und Abrechnungs-Partner</h3>
+    <br />
+    <p style="font-size: medium; line-height: normal">
+      Mit SportButler hast du einen zuverlässigen Partner an deiner Seite, der es dir ermöglicht, Sportevents reibungslos zu organisieren und die
+      Abrechnung im Handumdrehen zu erledigen. Nie war es einfacher, Sportveranstaltungen zu planen und die finanzielle Seite im Blick zu behalten.
+    </p>
+    <h3>Was bietet SportButler?</h3>
+    <ul>
+      <li>
+        <b>Eventplanung und -verwaltung:</b>
+        Organisiere Events aller Sport Arten, sei es ein Turnier, ein Wettbewerb oder ein Treffen mit Freunden, und verwalte alle Details an einem
+        Ort.
+      </li>
+      <li>
+        <b>Teilnehmerverwaltung:</b>
+        Behalte den Überblick über die Teilnehmer, verwalte Anmeldungen und stelle sicher, dass jeder genau weiß, wo und wann das Event stattfindet.
+      </li>
+      <li>
+        <b>Abrechnung und Finanzverwaltung:</b>
+        Vereinfache die Abrechnung, verfolge Einnahmen und Ausgaben, und behalte die finanzielle Gesundheit deiner Sportevents im Blick.
+      </li>
+      <li>
+        <b>Kommunikation:</b>
+        Nutze unsere integrierten Kommunikationstools, um Teilnehmer zu informieren, Fragen zu beantworten und Updates zu teilen.
+      </li>
+      <li>
+        <b>Berichte und Analysen:</b>
+        Erhalte wertvolle Einblicke in die Performance deiner Events und nimm fundierte Entscheidungen für die Zukunft.
+      </li>
+    </ul>
+
+    <b>Unsere Mission</b>
+    <br />
+    Unsere Mission bei SportButler ist es, die Organisation von Sportevents so unkompliziert wie möglich zu gestalten. Wir verstehen, dass Zeit und
+    Ressourcen kostbar sind, und unser Ziel ist es, dir zu helfen, mehr Zeit auf dem Spielfeld und weniger Zeit in der Verwaltung zu verbringen.
+    <br />
+    <br />
+
+    <b>Bereit, loszulegen?</b>
+    <br />
+    Registriere dich noch heute bei SportButler und erlebe, wie mühelos die Organisation und Abrechnung von Sportevents sein kann. Lass uns gemeinsam
+    die Welt des Sports organisieren und dabei Zeit und Energie sparen! Wir freuen uns darauf, dich bei der Organisation und Abrechnung deiner
+    nächsten Sportevents zu unterstützen. Sportliche Grüße, Das SportButler-Team
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+</script>
+
+<style scoped></style>
