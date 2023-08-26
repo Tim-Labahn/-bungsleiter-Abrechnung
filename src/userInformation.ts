f3db6287ffc7cc6699a474ab4dcf0d06bf3f21e2
Mister Obviusly <tim.labahn@freaknet.de>
2023-08-26 21:44:08 +0200

    last one for today

--- src/userInformation.ts
+++ src/userInformation.ts
@@ -11,9 +11,5 @@ export type UserType = {
 
 export const logedInUserID = ref<number | undefined>(80085);
 export const users = ref<UserType[]>([]);
-// users.value.push(logedInUserID.value);
 
 localStorage.setItem('LocalUserList', JSON.stringify(users.value));
-
-// let LocalUsers = JSON.parse(localStorage.getItem('Users')!);
-// let activUser = LocalUsers.find(user => user.ID === logedInUserID);
