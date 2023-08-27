eb2b48f074698ac01e979f3efa0f122287a329d3
Mister Obviusly <tim.labahn@freaknet.de>
2023-08-27 13:49:59 +0200

    logout

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -5,43 +5,69 @@
       style="
         position: absolute;
         margin-left: -700px;
-        margin-top: -200px;
+        margin-top: -250px;
         border: 1px solid #213547;
         background-color: whitesmoke;
         border-radius: 30px;
-        text-align: center;
         width: 200px;
-        height: 800px;
+        height: 870px;
         font-size: larger;
         text-align: left;
       "
-    ></div>
+    >
+      <p style="margin-left: 10px">Profil 👤</p>
+      <p style="margin-left: 10px">Settings ⚙️</p>
+      <p @click="logOutUser = true" style="margin-left: 10px">Logout ⚙️</p>
+
+      <p style="margin-left: 10px">Stats ⚙️</p>
+    </div>
 
     <div v-if="logedInUserID">
-      <div
-        style="
-          border: 1px solid #213547;
-          background-color: whitesmoke;
-          border-radius: 30px;
-          text-align: center;
-          width: 500px;
-          height: 400px;
-          font-size: larger;
-          text-align: left;
-        "
-      >
-        <h2 style="text-align: center">Account</h2>
-        <div style="margin-left: 20px">
-          <br />
-          User Name: {{ logedInUserData?.Name }}
-          <br />
-          Age: {{ logedInUserData?.Birthday.toString().substring(10, 8) }}. {{ logedInUserData?.Birthday.toString().substring(7, 5) }}.
-          {{ logedInUserData?.Birthday.toString().substring(4, 0) }}
-          <br />
-          Gender: {{ logedInUserData?.Gender }}
-          <br />
-          Passwort: {{ logedInUserData?.Passwort }}
-          <br />
+      <div v-if="logOutUser">
+        <div
+          style="
+            border: 1px solid #213547;
+            background-color: whitesmoke;
+            border-radius: 30px;
+            text-align: center;
+            width: 500px;
+            height: 400px;
+            font-size: larger;
+            text-align: left;
+          "
+        >
+          <h2 style="text-align: center">Account</h2>
+          <div>Are You Sure that you want to log out ?</div>
+          <button @click="logedInUserID = undefined">Fortfahren</button>
+          <button @click="logOutUser = false">Abbrechen</button>
+        </div>
+      </div>
+      <div v-if="!logOutUser">
+        <div
+          style="
+            border: 1px solid #213547;
+            background-color: whitesmoke;
+            border-radius: 30px;
+            text-align: center;
+            width: 500px;
+            height: 400px;
+            font-size: larger;
+            text-align: left;
+          "
+        >
+          <h2 style="text-align: center">Account</h2>
+          <div style="margin-left: 20px">
+            <br />
+            User Name: {{ logedInUserData?.Name }}
+            <br />
+            Age: {{ logedInUserData?.Birthday.toString().substring(10, 8) }}. {{ logedInUserData?.Birthday.toString().substring(7, 5) }}.
+            {{ logedInUserData?.Birthday.toString().substring(4, 0) }}
+            <br />
+            Gender: {{ logedInUserData?.Gender }}
+            <br />
+            Passwort: {{ logedInUserData?.Passwort }}
+            <br />
+          </div>
         </div>
       </div>
     </div>
@@ -72,7 +98,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import { UserType, users, logedInUserID } from '../userInformation';
-
+let logOutUser = ref(false);
 let logedInUserData = ref<UserType>();
 function loadUserData() {
   logedInUserData.value = users.value.find(user => user.ID === logedInUserID.value);
